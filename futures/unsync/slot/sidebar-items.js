3cbc275a50d68177c661c70efdf99a8f8694865e
Travis CI User <travis@example.org>
2017-09-24 14:49:10 +0000

    Documentation for alexcrichton/futures-rs@e5447d1

---
+++ futures/unsync/slot/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["channel","Creates an in-memory Stream which only preserves last value"]],"struct":[["Receiver","The receiving end of a channel which preserves only the last value"],["SendError","Error type for sending, used when the receiving end of a channel is dropped"],["Sender","Slot is very similar to unbounded channel but only stores last value sent"]]});
